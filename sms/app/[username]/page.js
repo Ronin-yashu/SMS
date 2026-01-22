58b1491a6fcede9a80838f863bba32664cbe828d
Ronin-yashu <yashu.jaat880@gmail.com>
2026-01-22 18:29:07 +0530

    login ui done

--- sms/app/[username]/page.js
+++ sms/app/[username]/page.js
@@ -1,5 +1,15 @@
+import { redirect } from 'next/navigation'
+import { getServerSession } from 'next-auth';
+
 const page = async ({params}) => {
     const { username } = await params;
+    const session = await getServerSession();
+    console.log("Session in user page:", session);
+    if (!session) {
+      // const session = await getServerSession();
+        console.log("No session found, redirecting to login");
+        return redirect('/login');
+    }
   return (
     <div>
         User Page for {username}
--- sms/app/login/page.js
+++ sms/app/login/page.js
@@ -2,7 +2,8 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { Login_Function } from '@/actions/Login_Function'
-import AuthClientPage from '@/components/AuthClientPage'
+import { FaGoogle , FaGithub} from "react-icons/fa";
+import Link from 'next/link'
 import { useSession, signIn, signOut } from "next-auth/react"
 import { useRouter } from 'next/navigation'
 
@@ -10,14 +11,17 @@ import { useRouter } from 'next/navigation'
 const Login = () => {
     const { data: session } = useSession()
     const router = useRouter();
-    if (session) {
-        router.push(`/${session.user.name}`);
-    }
+    React.useEffect(() => {
+        if (session) {
+            router.push(`/${session.user.name}`);
+        }
+    }, [session])
+
     const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm();
     return (
         <div className='w-full h-[89vh]'>
 
-            <div className='w-full h-1/4 flex flex-col justify-center items-center bg-orange-300'>
+            <div className='w-full h-1/4 flex flex-col justify-center items-center'>
                 <div className='text-4xl font-bold '>
                     <span className='text-blue-600'>O</span>
                     <span className='text-blue-900'>z</span>
@@ -28,10 +32,10 @@ const Login = () => {
                 <span className='text-slate-300'>10 : 45</span>
             </div>
 
-            <div className='w-full h-3/4 flex bg-red-400 '>
+            <div className='w-full h-3/4 flex justify-center items-center'>
 
-                <div className='w-1/2 h-full flex justify-center items-center bg-red-300'>
-                    <form className='bg-amber-900 flex flex-col w-2/3 h-3/4 gap-4 justify-center items-center rounded-lg' onSubmit={handleSubmit((data) => Login_Function(data))}>
+                <div className='w-1/2 h-full flex justify-center items-center'>
+                    <form className='flex flex-col w-2/3 h-3/4 gap-6 justify-center items-center rounded-lg' onSubmit={handleSubmit((data) => Login_Function(data))}>
                         <h2 className='text-2xl font-semibold'>Login using your credentials</h2>
                         <input className='border border-slate-400 py-2 text-black px-13 rounded-2xl' {...register("email", { required: true, message: "Email is required" })} type='email' placeholder='Enter your Email Address' />
                         {errors.email && <span className='text-red-600'>Email is required</span>}
@@ -41,8 +45,22 @@ const Login = () => {
                     </form>
                 </div>
 
-                <div className='w-1/2 h-full'>
-                    <AuthClientPage />
+                <div className="w-px h-3/4 bg-gray-400"></div>
+
+                <div className='w-1/2 h-full flex flex-col justify-center items-center gap-6'>
+                    <h2 className='text-2xl font-semibold'>Login with your providers</h2>
+                    <button onClick={()=>signIn("github")} className='cursor-pointer flex justify-center items-center gap-2 bg-slate-100 py-3 w-64 rounded'>
+                        <FaGithub />
+                        Login with GitHub
+                    </button>
+                    <button onClick={()=>signIn("google")} className='cursor-pointer flex justify-center items-center gap-2 bg-slate-100 py-3 w-64  rounded'>
+                        <FaGoogle />
+                        Login using Google
+                    </button>
+                    <div className='flex gap-32 mt-6'>
+                        <Link className='text-blue-600' href="/register">Don't have an account? Sign Up</Link>
+                        <Link className='text-blue-600' href="/forgot-password">Forgot Password ?</Link>
+                    </div>
                 </div>
             </div>
 
