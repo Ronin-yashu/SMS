165402978e2e154d529beca05c495481c06ff658
Ronin-yashu <yashu.jaat880@gmail.com>
2026-03-10 02:26:52 +0530

    Update GetAcademicYears.js

--- sms/actions/GetAcademicYears.js
+++ sms/actions/GetAcademicYears.js
@@ -10,13 +10,7 @@ export default async function GetAcademicYears(username) {
             }
         });
         if (!school) return null
-        const feeStructure = await prisma.feeStructure.findMany({
-            where: {
-                schoolId: school.id
-            }
-        })
-        if (!feeStructure) return null
-        const academic_years = await prisma.feeStructure.findMany({
+        const academicYears = await prisma.feeStructure.findMany({
             where: {
                 schoolId: school.id
             },
@@ -24,6 +18,11 @@ export default async function GetAcademicYears(username) {
                 academicYear: true,
             },
         })
+        if (!academicYears || academicYears.length == 0) return null
+        const academic_years = [...new Set(
+            feeStructure.map(f => f.academicYears)
+        )]
+
         return academic_years
     } catch (error) {
         console.log(error, "\n from check fee structure function");
