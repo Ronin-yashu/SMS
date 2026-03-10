66ce8f8a860eb300ecc860eb0f89156ec9eceab6
Ronin-yashu <yashu.jaat880@gmail.com>
2026-03-10 22:10:18 +0530

    Update prisma.js

--- sms/lib/prisma.js
+++ sms/lib/prisma.js
@@ -2,9 +2,13 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../prisma/generated/client/index.js";
 
-
 const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({adapter});
 
-export {prisma};
+const globalForPrisma = globalThis;
+
+if (!globalForPrisma.prisma) {
+  const adapter = new PrismaPg({ connectionString });
+  globalForPrisma.prisma = new PrismaClient({ adapter });
+}
+
+export const prisma = globalForPrisma.prisma;
